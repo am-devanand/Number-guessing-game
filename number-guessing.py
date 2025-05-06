2838291b77b6c5d18f6ffd63903c6cd88c976eee
am-devanand <amdevanand206@gmail.com>
2025-05-06 18:54:14 +0530

    number-guessing.py

--- number-guessing.py
+++ number-guessing.py
@@ -1,24 +1,21 @@
-import random
+import random 
 
 def number_guessing_game():
-    number_to_guess = random.ranint(1, 100)
+    number = random.randint(1, 10)
     attempts = 0
 
-    print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 100.")
+    print("Guess the number between 1 and 10.")
 
     while True:
         try:
-            guess = int(input("Take a guess: "))
+            guess = int(input("Your guess: "))
             attempts += 1
 
-            if guess < number_to_guess:
-                print("you are wrong,try again.")
-            elif guess > number_to_guess:
-                print("you are wrong. Try again.")
-            else:
-                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            if guess == number:
+                print(f"Correct! You guessed it in {attempts} attempts.")
                 break
+            else:
+                print("You are wrong. Try again.")
         except ValueError:
             print("Please enter a valid number.")
 
